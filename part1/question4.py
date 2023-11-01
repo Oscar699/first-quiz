5513ecb4c7e37b9ea0eded22f464f10e576cf172
Oscar699 <odrojasp@udistrital.edu.co>
2023-10-31 23:45:18 -0500

    Solution to question4

--- part1/question4.py
+++ part1/question4.py
@@ -11,7 +11,7 @@ import pets_db
 ################################################################################
 #
 # Instructions:
-# Question 4 and Question 5 are about writing SQL. THey use the database that is 
+# Question 4 and Question 5 are about writing SQL. They use the database that is 
 # created in the file `pets_db.py`. 
 # These questions use a database called SQLite. You do not need to install anything.
 # In the file `pets_db.py` three tables are created. Data is then added to each 
@@ -23,9 +23,10 @@ import pets_db
 # The output should be a list of tuples in the format: (<pet name>, <species>, <age>)
 
 sql_pets_owned_by_nobody = """
-
-Your SQL here.
-
+    SELECT A.name, A.species, A.age 
+    FROM animals A 
+    LEFT JOIN people_animals PA ON A.animal_id = PA.pet_id 
+    WHERE PA.pet_id IS NULL;
 """
 
 # Part 4.B:
@@ -33,16 +34,26 @@ Your SQL here.
 # The output should be an integer.
 
 sql_pets_older_than_owner = """
-
-Your SQL here.
-
+    SELECT COUNT(DISTINCT(A.animal_id)) 
+    FROM animals A 
+    JOIN people_animals PA ON A.animal_id = PA.pet_id
+    JOIN people P ON PA.owner_id = P.person_id
+    WHERE A.age > P.age;
 """
 
 # Part 4.C: BONUS CHALLENGE! 
 # Write SQL to select the pets that are owned by Bessie and nobody else.
 # The output should be a list of tuples in the format: (<person name>, <pet name>, <species>)
 sql_only_owned_by_bessie = """ 
-
-Your SQL here.
-
+    SELECT P.name, A.name, A.species
+    FROM animals A, people_animals PA, people P
+    WHERE A.animal_id = PA.pet_id 
+      AND PA.owner_id = P.person_id
+      AND P.person_id = 2
+      AND A.animal_id NOT IN (
+        SELECT A.animal_id 
+        FROM animals A, people_animals PA
+        WHERE A.animal_id = PA.pet_id
+          AND PA.owner_id != 2
+      );
 """
